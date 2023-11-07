c3bdff2ab7d65e75488ab3a307f5c02df08b8550
Eecodev <mnasser.bbk@gmail.com>
2023-11-07 16:18:40 +0100

    response property to return email value

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            emails: []
+            emails: [],
         }
     },
     methods:{
         getEmails(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail?qty=10').then((res)=>{
-                console.log(res.data.emails);
-                this.emails = res.data.emails;
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail?qty=10').then((res) => {
+                console.log(res.data.response);
+                this.emails = res.data.response;
             })
         }
     },
