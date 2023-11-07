60538c2323b1ff54f81042e7e3035dd991f60018
Eecodev <mnasser.bbk@gmail.com>
2023-11-07 16:55:35 +0100

    put axios.get inside loop to generate 10 emails

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ createApp({
     },
     methods:{
         getEmails(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                console.log(response.data.response);
-                this.emails.push(response.data);
-                this.currentIndex;
-            })
+                for(let i = 0; i < 10; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    console.log(res.data.response);
+                    this.emails.push(res.data.response);
+                })
+            }
         }
     },
     mounted(){
