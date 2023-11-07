7824c14db5202e42b751fd6fae53944b5c1f3baa
Eecodev <mnasser.bbk@gmail.com>
2023-11-07 17:26:21 +0100

    created fetchPromises empty array to keep track of API requests

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ createApp({
     data(){
         return{
             emails: [],
-            currentIndex: 0
+            currentIndex: 0,
+            fetchPromises: [],
+            allEmailsFetched: false
         }
     },
     methods:{
         getEmails(){
                 for(let i = 0; i < 10; i++){
-                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-                    console.log(res.data.response);
-                    this.emails.push(res.data.response);
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    console.log(response.data.response);
+                    this.emails.push(response.data.response);
+
+                    if(this.emails.length === 10){
+                        this.allEmailsFetched = true;
+                    }
                 })
+                fetchPromises.push(promise);
             }
+            Promise.all(fetchPromises).then(() => {
+                this.allEmailsFetched = true;
+            });
         }
     },
     mounted(){
